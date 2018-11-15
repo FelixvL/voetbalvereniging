836d583259ea4809a012064796a98631e847ba88
FelixvL <digitaleservice@gmail.com>
2018-11-15 10:02:59 +0100

    wedstrijden en contributie

--- Vereniging.java
+++ Vereniging.java
@@ -6,10 +6,35 @@ public class Vereniging {
 	ArrayList<Lid> leden = new ArrayList();
 	int aantalBezoekersInStadion;
 	String naam;
+	int hoogteContributiePerMaand;
+	int kasInhoud;
 	
 	void toonAlleLeden() {
 		for(Lid lid : leden) {
 			System.out.println(lid.naam + ">>" + lid.maandenAchterStand);
 		}
+	}
+	int berekenContributieSchuld() {
+		int totaal = 0;
+		for(Lid lid : leden) {
+			totaal = totaal + lid.maandenAchterStand * hoogteContributiePerMaand;
+		}
+		return totaal;
+	}
+	Vereniging(String naam, int stadionCapaciteit, int contributie){
+		this.naam = naam;
+		aantalBezoekersInStadion = stadionCapaciteit;
+		hoogteContributiePerMaand = contributie;
+		
+	}
+	void wedstrijdSpelen(int prijs) {
+		System.out.println("jojo in wedstrijdspelen"+prijs*aantalBezoekersInStadion);
+		kasInhoud = kasInhoud + prijs * aantalBezoekersInStadion;
+	}
+	int toonKasInhoud() {
+		System.out.println("In de kas zit" + kasInhoud);
+		System.out.println("Van de leden krijgen wij nog" + berekenContributieSchuld());
+		System.out.println("Het totaal is dus: "+ (kasInhoud + berekenContributieSchuld()));
+		return kasInhoud;
 	}
 }
